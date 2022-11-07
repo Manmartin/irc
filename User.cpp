08b4ff68a5673cb5b502b37a255a22eab630e6d5
albgarci <albgarci@student.42madrid.com>
2022-11-07 23:55:44 +0100

    Refactor: user, force 4 params

--- User.cpp
+++ User.cpp
@@ -11,10 +11,11 @@ User::~User(void)
 
 void User::exec(std::string instruction, Client& client)
 {
-	std::list<std::string>				values;
+	std::vector<std::string>				values;
 	std::list<std::string>::iterator	it;
 	std::string							params;
 	std::string							realName;
+	size_t								pos;
 
 	if (!client.isChallengePassed())
 	{
@@ -23,16 +24,17 @@ void User::exec(std::string instruction, Client& client)
 	}
 	else if (client.isRegistered())
 		return (client.sendReply(ERR_ALREADYREGISTERED(client.getNickname())));
+	pos = instruction.find(":");
 	params = instruction.substr(0, instruction.find(":"));
 	realName = instruction.substr(instruction.find(":") + 1, instruction.size() - 1);
-	values = split_cpp(params, ' ');
-	it = values.begin();
-	if (values.size() < 3 || realName.size() < 1 || (*it).size() < 1)
+	values = splitToVector(trimSpaces(params), ' ');
+	if (values.size() < 3 || pos == std::string::npos)
 		return (client.sendReply(ERR_NEEDMOREPARAMS(client.getNickname(), "USER")));
-	client.setUser(*it);
-	it++; 
-	it++;
-	client.setRealName(realName);
+	client.setUser(values[0]);
+	if (values.size() == 4)
+		client.setRealName(values[3]);
+	else
+		client.setRealName(realName);
 	if (client.getNickname().size() > 0 && client.isPassOk())
 		this->server->registerAndWelcome(client);
 }
